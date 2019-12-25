26bbd7753b876b10f64011a9d24765b77f0e1733
Hael Chan <f.procumbens@gmail.com>
2019-12-24 23:02:42 -0800

    period and comma for number representation

--- portrait/calc-script.js
+++ portrait/calc-script.js
@@ -2,23 +2,27 @@ const screen = document.querySelector(".screen");
 let runningTotal;
 let buffer;
 let previousOperator;
+let containPeriod;
 
 init();
 
 function init() {
   runningTotal = 0;
   buffer = "0";
-  bufferValue = 0;
   previousOperator = null;
+  containPeriod = false;
 }
 
 document.querySelector('.buttons').addEventListener('click', (event) => {
-  // console.log(event.target.innerText);
   buttonClick(event.target.innerText);
 })
 
 function buttonClick(value) {
-  if (isNaN(parseInt(value))) {
+  if (value.length > 3) {
+    return;
+  }
+  
+  if (value !== "." && isNaN(parseInt(value))) {
     handleSymbol(value);
   }
   else {
@@ -32,17 +36,38 @@ function handleSymbol(value) {
 }
 
 function handleNumber(value) {
-  if (buffer === "0") {
-    buffer = value;
+  if (value === ".") {
+    addPeriod();
   }
-  else if (buffer.length < 11){
-    buffer += value;
-    buffer = buffer.replace(/,/g, "");
-    if (buffer.length > 6) {
-      buffer = buffer.substr(0, buffer.length - 6) + "," + buffer.substr(buffer.length - 6, 3) + "," + buffer.substr(buffer.length - 3);
-    } else if (buffer.length > 3) {
-      buffer = buffer.substr(0, buffer.length - 3) + "," + buffer.substr(buffer.length - 3);
+  else {
+    if (buffer === "0") {
+      buffer = value;
     }
+    else {
+      let digitNum = buffer.replace(/[,\.]/g, "").length;
+      if (digitNum < 9) {
+        buffer += value;
+        if (!containPeriod) {
+          addComma();
+        }
+      }
+    }
+  }
+}
+
+function addComma() {
+  buffer = buffer.replace(/,/g, "");
+  if (buffer.length > 6) {
+    buffer = buffer.substr(0, buffer.length - 6) + "," + buffer.substr(buffer.length - 6, 3) + "," + buffer.substr(buffer.length - 3);
+  } else if (buffer.length > 3) {
+    buffer = buffer.substr(0, buffer.length - 3) + "," + buffer.substr(buffer.length - 3);
+  }
+}
+
+function addPeriod() {
+  if (!containPeriod && buffer.length < 11) {
+    containPeriod = true;
+    buffer += ".";
   }
 }
 
